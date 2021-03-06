2b25cc16152c6702ce294fec35d129c4f3f40c7a
unknown <fgdesignworld@gmail.com>
2021-03-06 14:05:58 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// features1git pull origin
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container">
+    <header><a href="/">React Shoping cart</a></header>
+    <main>
+      Prodcuts List
+    </main>
+    <footer>All Copie Rights Recived</footer>
+  </div>
   );
 }
 
